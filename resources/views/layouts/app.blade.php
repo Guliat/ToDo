8775348b95da8b7ac3f3b8aef68553818120ff9a
AleksandarEnchev <guliat88@mail.bg>
2020-10-29 13:51:52 +0200

    start new auth pages, styles | some fixes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,90 +1,37 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 	<head>
+		<!-- Meta -->
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<!-- CSRF Token -->
 		<meta name="csrf-token" content="{{ csrf_token() }}">
+		<!-- Title -->
 		<title>{{ config('app.name', 'Laravel') }}</title>
-		<!-- Scripts -->
-		<script src="https://cdn.jsdelivr.net/npm/pikaday/pikaday.js"></script>
 		<!-- Fonts -->
-		<link href="https://fonts.googleapis.com/css2?family=Caveat&display=swap" rel="stylesheet">
+	  <link href="https://fonts.googleapis.com/css2?family=Exo+2:wght@300&display=swap" rel="stylesheet">
+	  <link href="https://fonts.googleapis.com/css2?family=Caveat&display=swap" rel="stylesheet">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css">
 		<!-- Styles -->
 		<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+		<script src="https://cdn.jsdelivr.net/npm/pikaday/pikaday.js"></script>
 		{{-- TODO: move only in needed views with yield 'styles' --}}
-			@livewireStyles
-			<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/pikaday/css/pikaday.css">
+		@livewireStyles
+		<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/pikaday/css/pikaday.css">
 		{{-- END TODO --}}
 	</head>
-	<body class="has-navbar-fixed-top">
-		<div class="container is-fluid">
-			{{-- TODO: move "nav" in partial view --}}
-			<nav class="navbar is-dark is-fixed-top" role="navigation" aria-label="main navigation">
-				<div class="navbar-brand">
-					<a class="ml-3 mt-2 is-size-4" style="font-family: Caveat;" href="{{ url('/') }}">
-						{{ config('app.name', 'Laravel') }}
-					</a>
-					<a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-						<span aria-hidden="true"></span>
-						<span aria-hidden="true"></span>
-						<span aria-hidden="true"></span>
-					</a>
-				</div>
-				<div id="navbarBasicExample" class="navbar-menu">
-					<div class="navbar-start ml-5">
-						<a href="{{ route('todo.index') }}" class="navbar-item">
-							Todos
-						</a>
-					</div>
-
-					<div class="navbar-end">
-						<div class="navbar-item">
-							<div class="buttons">
-								<!-- Authentication Links -->
-								@guest
-									<a class="button is-light" href="{{ route('login') }}">{{ __('Login') }}</a>
-									@if (Route::has('register'))
-										<a class="button is-success" href="{{ route('register') }}">{{ __('Register') }}</a>
-									@endif
-								@endguest
-							</div>
-						</div>
-						<div class="navbar-item">
-							@if(Auth::check())
-								<div class="navbar-item has-dropdown is-hoverable">
-									<a class="navbar-link">
-											{{ Auth::user()->name }} {{ Auth::user()->lastname }}
-									</a>
-									<div class="navbar-dropdown">
-										<a href="#" class="navbar-item is-size-5">
-											---
-										</a>
-										<a href="#" class="navbar-item is-size-5">
-											---
-										</a>
-										<form action="{{ route('logout') }}" method="POST">
-											@csrf
-											<button type="submit" class="button is-text is-size-5" href="{{ route('logout') }}">
-												{{ __('Logout') }}
-											</button>
-										</form>
-									</div>
-								</div>
-							@endif
-						</div>
-					</div>
-				</div>
-			</nav>
-			<div class="py-4">
-					@yield('content')
+	<body>
+		<div class="columns">
+			<div class="column is-2">
+				@yield('sidemenu')
+			</div>
+			<div class="column is-10">
+				@yield('content')
 			</div>
 		</div>
 		<script src="{{ asset('js/app.js') }}"></script>
+		@yield('scripts')
 		{{-- TODO: move only in needed views with yield 'scripts' --}}
 			@livewireScripts
 		{{-- END TODO --}}
-		@yield('scripts')
 	</body>
 </html>
--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -1,82 +1,118 @@
 @extends('layouts.app')
 
-@section('content')
-<div class="columns is-multiline" id="todos">
-	<div class="column is-2">
-		<a href="{{ route('todo.create') }}" class="button is-success is-medium">
-			<span class="icon"><i class="fa fa-plus"></i></span>
-			<span>Add new Todo</span>
-		</a>
-	</div>
-	<div class="column is-8 is-size-1 has-text-centered" style="font-family: Caveat;">
-		ToDo List
-	</div>
-	<div class="column is-6">
-    <div>
-      <form action="{{ route('todo.search') }}" method="post">
-      @csrf
-        <b-field label="Find a ToDo">
-          <b-autocomplete
-          rounded
-          name="todo"
-          icon-pack="fa"
-          icon="magic"
-          v-model="todo_name"
-          :data="filteredExchangeArray"
-          placeholder="autocomplete field"
-          open-on-focus="openOnFocus"
-          clearable="clearable"
-          icon-right="caret-down"
-          @select="option => selected = option">
-          </b-autocomplete>
-          <template slot="empty">No results found</template>
-        </b-field>
-        <button type="submit" class="button is-success is-rounded">Go to this ToDo</button>
+@section('sidemenu')
+<aside class="menu pt-6 pl-5">
+  <p class="menu-label">
+    General
+  </p>
+  <ul class="menu-list">
+    <li><a class="sidemenu_background has-text-white">Home</a></li>
+  </ul>
+  <p class="menu-label">
+    Categories
+  </p>
+  <ul class="menu-list">
+    <li><a>Invitations</a></li>
+    <li><a>Cloud Storage </a></li>
+    <li><a>Authentication</a></li>
+  </ul>
+  <p class="menu-label">
+    Statuses
+  </p>
+  <ul class="menu-list">
+    <li><a>New</a></li>
+    <li><a>Important</a></li>
+    <li><a>Completed</a></li>
+  </ul>
+  <ul class="menu-list">
+    <li>
+      <form action="{{ route('logout') }}" method="POST">
+		  @csrf
+		    <button type="submit" class="button is-text" href="{{ route('logout') }}">Logout</button>
       </form>
+    </li>
+  </ul>
+</aside>
+@endsection
+@section('content')
+<div class="container pt-5">
+  <div class="columns is-multiline" id="todos">
+    <div class="column is-2">
+      <a href="{{ route('todo.create') }}" class="button is-success is-medium">
+        <span class="icon"><i class="fa fa-plus"></i></span>
+        <span>Add new Todo</span>
+      </a>
     </div>
-	</div>
-	<div class="column is-6 has-text-right">
-		<div class="dropdown is-hoverable is-right">
-			<div class="dropdown-trigger">
-				<button class="button" aria-haspopup="true" aria-controls="dropdown-menu4">
-					<span>Filters</span>
-					<span class="icon is-small">
-						<i class="fas fa-filter" aria-hidden="true"></i>
-					</span>
-				</button>
-			</div>
-			<div class="dropdown-menu" id="dropdown-menu4" role="menu">
-				<div class="dropdown-content">
-					<div class="dropdown-item">
-            @if(Session::get('sort_name') == 'asc') 
-            <form method="post" action="{{ route('sort.name.desc') }}">
-              @csrf
-              @method('put')
-              <button type="submit" class="button is-white" >
-              <span>Name</span>
-              <span class="icon">
-                <i class="fa fa-arrow-down"></i>
-              </span>
-              </button>
-            </form>
-            @else
-            <form method="post" action="{{ route('sort.name.asc') }}">
-              @csrf
-              @method('put')
-              <button type="submit" class="button is-white" >
+    <div class="column is-8 is-size-1 has-text-centered" style="font-family: Caveat;">
+      ToDo List
+    </div>
+    <div class="column is-6">
+      <div>
+        <form action="{{ route('todo.search') }}" method="post">
+        @csrf
+          <b-field label="Find a ToDo">
+            <b-autocomplete
+            rounded
+            name="todo"
+            icon-pack="fa"
+            icon="magic"
+            v-model="todo_name"
+            :data="filteredExchangeArray"
+            placeholder="autocomplete field"
+            open-on-focus="openOnFocus"
+            clearable="clearable"
+            icon-right="caret-down"
+            @select="option => selected = option">
+            </b-autocomplete>
+            <template slot="empty">No results found</template>
+          </b-field>
+          <button type="submit" class="button is-success is-rounded">Go to this ToDo</button>
+        </form>
+      </div>
+    </div>
+    <div class="column is-6 has-text-right">
+      <div class="dropdown is-hoverable is-right">
+        <div class="dropdown-trigger">
+          <button class="button" aria-haspopup="true" aria-controls="dropdown-menu4">
+            <span>Filters</span>
+            <span class="icon is-small">
+              <i class="fas fa-filter" aria-hidden="true"></i>
+            </span>
+          </button>
+        </div>
+        <div class="dropdown-menu" id="dropdown-menu4" role="menu">
+          <div class="dropdown-content">
+            <div class="dropdown-item">
+              @if(Session::get('sort_name') == 'asc') 
+              <form method="post" action="{{ route('sort.name.desc') }}">
+                @csrf
+                @method('put')
+                <button type="submit" class="button is-white" >
                 <span>Name</span>
                 <span class="icon">
-                  <i class="fa fa-arrow-up"></i>
+                  <i class="fa fa-arrow-down"></i>
                 </span>
-              </button>
-            </form>
-            @endif
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	<livewire:crud />
+                </button>
+              </form>
+              @else
+              <form method="post" action="{{ route('sort.name.asc') }}">
+                @csrf
+                @method('put')
+                <button type="submit" class="button is-white" >
+                  <span>Name</span>
+                  <span class="icon">
+                    <i class="fa fa-arrow-up"></i>
+                  </span>
+                </button>
+              </form>
+              @endif
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <livewire:crud />
+  </div>
 </div>
 @endsection
 @section('scripts')
